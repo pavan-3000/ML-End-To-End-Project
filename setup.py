b35b7f35e36f4e7bcf42906253c89580b3d4022b
pavan-3000 <pavansingh3000@gmail.com>
2023-12-11 17:47:42 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="ML end-to-end project",
+    version="0.0.0",
+    author="pavan",
+    author_email="pavansingh3000@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
